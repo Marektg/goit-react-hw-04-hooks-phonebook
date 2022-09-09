609ec0bb655e67f2cab25fe8483f1880e338958d
Marek Kalus <93349057+Marektg@users.noreply.github.com>
2022-09-09 10:32:29 +0200

    Update Phonebook.jsx

--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -17,10 +17,10 @@ function getFromLocalStorage(key) {
 let localContacts = getFromLocalStorage("contacts");
 if (localContacts === null) {
     localContacts = [
-        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+        { id: 'nanoid()', name: 'Rosie Simpson', number: '459-12-56' },
+        { id: 'nanoid()', name: 'Hermione Kline', number: '443-89-12' },
+        { id: 'nanoid()', name: 'Eden Clements', number: '645-17-79' },
+        { id: 'nanoid()', name: 'Annie Copeland', number: '227-91-26' },
     ] };
 
 class Phonebook extends React.Component {
